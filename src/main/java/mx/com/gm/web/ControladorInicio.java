706cd88ddbb38ea719ecc37ea1af3e116e45aa06
Pedro Ivan <pedroivanlarios@gmail.com>
2023-02-28 12:53:54 -0600

    Tercer de Control

--- src/main/java/mx/com/gm/web/ControladorInicio.java
+++ src/main/java/mx/com/gm/web/ControladorInicio.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+
 //import java.util.Arrays;
 //import java.util.ArrayList;
 
@@ -39,6 +40,17 @@ public class ControladorInicio {
     @PostMapping("/guardar")
     public String guardar(Persona persona){
         personaService.guardar(persona);
+        return "redirect:/";
+    }
+    @GetMapping("/editar/{idPersona}")
+    public String editar(Persona persona, Model model){
+        persona = personaService.encontrarPersona(persona);
+        model.addAttribute("persona", persona);
+        return "modificar";
+    }
+    @GetMapping("/eliminar")
+    public String eliminar(Persona persona){
+        personaService.eliminar(persona);
         return "redirect:/";
     }
 }
